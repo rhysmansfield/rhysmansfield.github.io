b4d0a65c415661c38d3d4fa64936dea390302a12
Rhys Mansfield <rhys.mansfield@heat.io>
2023-08-15 11:37:45 +0100

    Made header animation desktop only

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ new Swiper(".collection__card-swiper", {
 });
 
 const header = document.querySelector(".collection__header");
-const animation = gsap.to(header, {
-  opacity: 0,
-  scrollTrigger: {
-    trigger: header,
-    start: `bottom+=75% center`,
-    end: `bottom+=150% center`,
-    scrub: true,
-  },
-});
+if (window.innerWidth >= 768) {
+  gsap.to(header, {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: header,
+      start: `bottom+=75% center`,
+      end: `bottom+=150% center`,
+      scrub: true,
+    },
+  });
+}
