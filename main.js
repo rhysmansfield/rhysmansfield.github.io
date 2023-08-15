563e82260dfd6b25dbc3a1b79049d09dc23d6860
Rhys Mansfield <rhys.mansfield@heat.io>
2023-08-15 11:33:57 +0100

    Added header & mobile click effect

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-new Swiper(".collection__card__swiper", {
+gsap.registerPlugin(ScrollTrigger);
+
+new Swiper(".collection__card-swiper", {
   slidesPerView: 1,
+});
+
+const header = document.querySelector(".collection__header");
+const animation = gsap.to(header, {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: header,
+    start: `bottom+=75% center`,
+    end: `bottom+=150% center`,
+    scrub: true,
+  },
 });
